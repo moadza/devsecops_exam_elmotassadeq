96899763fc8fbbcb2f3a638837341c4547df069a
moadza <moadza1928@gmail.com>
2024-10-28 09:37:31 +0100

    Project intialisation

---
+++ src/main/java/com/ensa/devsecops_exam_moad/DevsecopsExamMoadApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.devsecops_exam_moad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsExamMoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsecopsExamMoadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ensa/devsecops_exam_moad/DevsecopsExamMoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.devsecops_exam_moad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsExamMoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
